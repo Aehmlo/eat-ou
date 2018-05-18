a16cd94dddcf9faf0e192cd2ccc03f2299b8d6f1
Alex Hamilton <alex.hamilton@ou.edu>
2018-05-17 22:51:46 -0500

    Add service worker and add to home screen scripts.
    Disable service worker for now to avoid caching headaches.

---
+++ static/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "eat-ou-v1";
+const urls = [
+  "/",
+  "/style.css",
+  "/eat-ou.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urls)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => (r || fetch(e.request))));
+})
