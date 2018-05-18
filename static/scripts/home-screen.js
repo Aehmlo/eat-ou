176637433333cd3c6927a7571d9d628eaddfce16
Alex Hamilton <alex.hamilton@ou.edu>
2018-05-17 23:19:56 -0500

    Finalize home screen functionality.
    Also update icons.

--- static/scripts/home-screen.js
+++ static/scripts/home-screen.js
@@ -2,7 +2,7 @@ window.addEventListener("beforeinstallprompt", e => {
 	e.preventDefault();
 	deferredPrompt = e;
 	let add = document.getElementById("add_button");
-	add.style.display = "block";
+	add.style.display = "initial";
 	add.addEventListener("click", v => {
 		add.style.display = 'none';
 		e.prompt();
